627ccc3fd1a0bf64bb56f29067ef74829770e7e0
dymx101 <yimingdong@Yimings-MacBook-Pro-2.local>
2019-05-08 07:58:33 +0800

    add project

---
+++ Example/Pods/Target Support Files/Pods-NovelScraper_Example/Pods-NovelScraper_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NovelScraper_Example : NSObject
+@end
+@implementation PodsDummy_Pods_NovelScraper_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-NovelScraper_Tests/Pods-NovelScraper_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NovelScraper_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_NovelScraper_Tests
+@end
